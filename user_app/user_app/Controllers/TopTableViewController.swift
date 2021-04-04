04e892b0fa96a7f7fd38ed62076743b98162a0e1
arai <niimaru09@gmail.com>
2021-04-04 11:48:37 +0900

    リスト作って表示するところまで

--- user_app/user_app/Controllers/TopTableViewController.swift
+++ user_app/user_app/Controllers/TopTableViewController.swift
@@ -8,82 +8,58 @@
 import UIKit
 
 class TopTableViewController: UITableViewController {
+    
+    
+    let user1 = userModel(name: "kousuke", birthday: "2020/10/29", job: "学生")
+    let user2 = userModel(name: "tom", birthday: "2020/10/29", job: "ニート")
+    let user3 = userModel(name: "さいおんじ", birthday: "2020/10/29", job: "サラリーマン")
+    
+    // 年齢、生年月日、職業
+    var userInfoList:[userModel] = []
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        userInfoList.append(user1)
+        userInfoList.append(user2)
+        userInfoList.append(user3)
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
-    // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return userInfoList.count
     }
-
-    /*
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+        
+        let userInfo = userInfoList[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as UITableViewCell?
+        let userNameLabel = cell?.contentView.viewWithTag(1) as! UILabel
+        let birthDayLabel = cell?.contentView.viewWithTag(2) as! UILabel
+        let jobLabel = cell?.contentView.viewWithTag(3) as! UILabel
+        
+        userNameLabel.text = userInfo.name
+        birthDayLabel.text = userInfo.birthday
+        jobLabel.text = userInfo.job
+        
+        return cell!
 
     }
-    */
 
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
+
+    @IBAction func buttonDidTapped(_ sender: Any) {
+        performSegue(withIdentifier: "goInputUserInfoVC", sender: nil)
     }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
