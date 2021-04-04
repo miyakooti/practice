eec69b54a1ab30e15413330f1a9a768424d33a07
arai <niimaru09@gmail.com>
2021-04-04 11:10:15 +0900

    user_app init

---
+++ user_app/user_app/Controllers/InputUserInfoViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  user_app
+//
+//  Created by Arai Kousuke on 2021/04/04.
+//
+
+import UIKit
+
+class InputUserInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
