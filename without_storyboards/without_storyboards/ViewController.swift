136929b23b3b729536d20a5fe16033c678f10da8
arai <niimaru09@gmail.com>
2021-05-24 22:47:58 +0900

    without storyboards init

---
+++ without_storyboards/without_storyboards/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  without_storyboards
+//
+//  Created by Arai Kousuke on 2021/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
